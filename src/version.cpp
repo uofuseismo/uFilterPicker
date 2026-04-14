1a46715b63d1f154d4b71a126bd231c0f5c0c0ad
Ben Baker <bakerb845@gmail.com>
2026-04-14 08:39:17 -0600

    getting closer - missing a version

---
+++ src/version.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include "uFilterPicker/version.hpp"
+
+using namespace UFilterPicker;
+
+int Version::getMajor() noexcept
+{
+    return uFilterPicker_MAJOR;
+}
+
+int Version::getMinor() noexcept
+{
+    return uFilterPicker_MINOR;
+}
+
+int Version::getPatch() noexcept
+{
+    return uFilterPicker_PATCH;
+}
+
+//NOLINTBEGIN(bugprone-easily-swappable-parameters)
+bool Version::isAtLeast(const int major, const int minor,
+                        const int patch) noexcept
+//NOLINTEND(bugprone-easily-swappable-parameters)
+{
+    if (uFilterPicker_MAJOR < major){return false;}
+    if (uFilterPicker_MAJOR > major){return true;}
+    if (uFilterPicker_MINOR < minor){return false;}
+    if (uFilterPicker_MINOR > minor){return true;}
+    if (uFilterPicker_PATCH < patch){return false;}
+    return true;
+}
+
+std::string Version::getVersion() noexcept
+{
+    std::string version{uFilterPicker_VERSION};
+    return version;
+}
+
